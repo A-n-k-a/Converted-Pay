f0eae255e64f96a31293621a49a66e46da908409
A-n-k-a <47747022+A-n-k-a@users.noreply.github.com>
2024-03-05 23:53:45 +0800

    Personalize changes

--- config.js
+++ config.js
@@ -3,12 +3,29 @@
 const basic = { // 基础设置
     favicon: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>👻</text></svg>', // 页面图标
     title: '(～￣▽￣)～', // 页面标题
-    avatar: 'https://gravatar.loli.net/avatar/', // 头像 URL
-    name: '这是一个昵称！', // 头像下的昵称
-    sign: '个性签名或者提示文字 ( •̀ ω •́ )✧', // 个性签名或提示文字，可使用 HTML 格式
-    user_page: 'https://github.com/ghost', // 非支付软件点击头像或名字时跳转链接。留空或删除则不跳转
-    footer: '这是一个页脚！', // 页脚文字，可使用 HTML 格式
-    uri_redirect: false // 若收款码 URL 是网址，是否直接跳转而不显示二维码
+    //avatar: 'https://gravatar.loli.net/avatar/', // 头像 URL
+    avatar: 'https://avatars.githubusercontent.com/u/47747022?v=4',
+    name: '不死鸟Anka', // 头像下的昵称
+    sign: '不知大海之宽阔，却知晓天空之蓝 ( •̀ ω •́ )✧', // 个性签名或提示文字，可使用 HTML 格式
+    user_page: 'https://github.com/A-n-k-a', // 非支付软件点击头像或名字时跳转链接。留空或删除则不跳转
+    footer: `<script>
+function show(){
+var date = new Date(); //日期对象
+var now = "";
+now = date.getFullYear()+"年"; //读英文就行了
+now = now + (date.getMonth()+1)+"月"; //取月的时候取的是当前月-1如果想取当前月+1就可以了
+now = now + date.getDate()+"日";
+now = now + date.getHours()+"时";
+now = now + date.getMinutes()+"分";
+now = now + date.getSeconds()+"秒";
+document.getElementById("nowDiv").innerHTML = now; //div的html是now这个字符串
+setTimeout("show()",1000); //设置过1000毫秒就是1秒，调用show方法
+}
+</script>
+<body οnlοad="show()"> <!-- 网页加载时调用一次 以后就自动调用了-->
+<div id="nowDiv"></div>
+</body>`, // 页脚文字，可使用 HTML 格式
+    uri_redirect: true // 若收款码 URL 是网址，是否直接跳转而不显示二维码
 }
 
 const theme = { // 主题设置
